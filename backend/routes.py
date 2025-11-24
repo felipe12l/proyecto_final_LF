0b1c7d9da52e88617da45ffc29871225c3634aa6
Nicolas Tinjaca <nicolas.tinjaca@uptc.edu.co>
2025-11-23 23:25:37 -0500

    (add) save test case in database

--- backend/routes.py
+++ backend/routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException
 from controllers.analyzeController import analyzeJWT
 from controllers.encodeController import encode_jwt
 from controllers.analyzeController import analyze_repository
+from database.db import save_analysis
 router = APIRouter()
 
 @router.post("/api/analyze")
@@ -9,7 +10,12 @@ def analyze(data: dict):
     token = data.get("token")
     if not token:
         raise HTTPException(400, "token requerido")
-    return analyzeJWT(token)
+
+    result = analyzeJWT(token)
+
+    if result.get("status") == "ok":
+        save_analysis(token, result)
+    return result
 
 
 @router.post("/api/encode")
